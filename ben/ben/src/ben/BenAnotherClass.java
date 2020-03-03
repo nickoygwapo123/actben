5814184f1c39f793de675d2e7b39e260145e3547
Ben <46435167+nickoygwapo123@users.noreply.github.com>
2020-03-03 23:30:22 +0800

    Add files via upload

---
+++ ben/ben/src/ben/BenAnotherClass.java
@@ -0,0 +1,13 @@
+package ben;
+
+public class BenAnotherClass {
+    private String msg = "Test from other class";
+    
+    public String getmsg(){
+    return this.msg;
+    }
+    
+    String display(){
+    return msg;
+    }
+}
---
+++ ben/ben/src/ben/BenStudentSys.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ben;
+
+/**
+ *
+ * @author User
+ */
+public class BenStudentSys {
+    
+    public static void main(String[] args){
+    BenStudentInfo s = new BenStudentInfo();
+        s.input();
+        BenStudentEnroll e = new BenStudentEnroll();
+        e.input2();
+    }
+}
